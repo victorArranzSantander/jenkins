09ab0fb53206c3f82d627ef6398e508252f7b664
Víctor <victor.arranz@gruposantander.com>
2023-01-10 09:16:58 +0100

    Cambios código react

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
-import './App.css';
-import Testimonio from './componentes/Testimonio';
-
+import "./App.css";
+import Hola from "./componentes/Hola";
 
 function App(props) {
   return (
     <div className="App">
-      <div className = "contenedor-testimonio">
-        <h1>Personas</h1>
-        <Testimonio 
-          nombre = "Walter White"
-          cargo = "Profesor de química"
-          imagen = "ww.webp"
-          texto = "TextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTexto"
-        />
-        <Testimonio 
-          nombre = "Antonio Recio"
-          cargo = "Mayorista no limpia pescado"
-          imagen = "recio.jpeg"
-          texto = "TextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTexto"
-        />
-        <Testimonio 
-          nombre = "Myke Wazowski"
-          cargo = "Asustador"
-          imagen = "myke.jpeg"
-          texto = "TextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTexto"
-        />
-        <Testimonio 
-          nombre = "Tyrion Lannister"
-          cargo = "Mano del rey"
-          imagen = "tyrion.webp"
-          texto = "TextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTextoTexto"
-        />
+      <div>
+        <Hola />
       </div>
     </div>
   );
---
+++ src/componentes/Hola.jsx
@@ -0,0 +1,7 @@
+import react from "react";
+
+function Hola() {
+  return <h1>Hola Jenkins!</h1>;
+}
+
+export default Hola;
